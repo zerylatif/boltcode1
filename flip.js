6cf13061a377d86e5cbc7d12d1fe3a0f479cfc31
ZERY LATIF <50826457+zerylatif@users.noreply.github.com>
2021-03-25 16:30:11 +0000

    Add files via upload

---
+++ flip.js
@@ -0,0 +1,12 @@
+
+  const card = document.querySelectorAll('.card__inner');
+
+  card.forEach((card) => {
+       card.addEventListener("click", () => {
+            card.classList.toggle("is-flipped");
+       });
+  });
+
+
+   
+
---
+++ main.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+
+        }else{
+            $('.navbar').removeClass("sticky");
+
+    }
+
+    });
+    // toggle menu script
+    $('.menu-button').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-button i').toggleClass("active");
+
+    });
+
+});
+
+    
